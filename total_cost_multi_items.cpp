2e8beae95dd3a83c6862302e2f0cf76c1f896885
Ramilbey <ramilbey505711503@gmail.com>
2025-10-04 21:18:20 +0800

    An optional damage protection (insurance) is offered for any item that costs more than RM250. The damage protection cost is 1% of the cost of the item.

--- total_cost_multi_items.cpp
+++ total_cost_multi_items.cpp
@@ -8,7 +8,6 @@ public:
     string freeShipping = "FREESHIPPING";
 };
 
-
 double shippingFee(string type) {
     if (type == "Saver")
         return 4.90;
@@ -19,7 +18,6 @@ double shippingFee(string type) {
     else
         return 0.0; 
 }
-
 
 double applyVoucher(string voucher, double itemCost) {
     VoucherCode myVoucher;
@@ -34,81 +32,94 @@ double applyVoucher(string voucher, double itemCost) {
             discount = itemCost * 0.10; 
         }
     } else if (voucher == myVoucher.freeShipping) {
-       
+        // no discount,
     }
 
     return discount;
 }
 
+double applyInsurance(double itemCost) {
+    return itemCost * 0.01;
+}
+
 int main() {
-    
     char choice;
     int size = 5;
-    double sumOfTransactions[size] ={};
+    double sumOfTransactions[size] = {};
     int counter = 0; 
-    double total =0.0;
-    do{
-       
+    double total = 0.0;
+
+    do {
         double itemCost;
         double sum = 0.0;
-        cout << "Enter the price of the item: (Enter -1 to stop) " ;
-        while ( true){
+        double insuranceTotal = 0.0;
+        cout << "Enter the price of the item: (Enter -1 to stop) ";
+
+        while (true) {
             cin >> itemCost;
-            if( itemCost == -1)
+            if (itemCost == -1)
                 break;
+
             sum += itemCost;
-        };
-        
+
+            if (itemCost >= 250) {
+                char insuranceAnswer;
+                cout << "Would you like to add damage protection insurance (y/n): ";
+                cin >> insuranceAnswer;
+                if (insuranceAnswer == 'y' || insuranceAnswer == 'Y') {
+                    double insurance = applyInsurance(itemCost);
+                    sum += insurance;
+                    insuranceTotal += insurance;  
+                }
+            }
+        }
+
         string shippingType;
         cout << "Enter shipping type (Saver / Standard / Courier): ";
         cin >> shippingType;
-    
+
         string voucher;
         cout << "Enter the voucher code: ";
         cin >> voucher;
-    
+
         double discount = applyVoucher(voucher, sum);
         double finalCost = sum - discount;
-    
+
         double fee = 0.0;
         if (voucher != "FREESHIPPING") {
             fee = shippingFee(shippingType);
         }
-    
+
         finalCost += fee;
         sumOfTransactions[counter] = finalCost;
         counter++;
-    
+
         cout << "Discount applied: RM " << discount << endl;
         cout << "Shipping fee: RM " << fee << endl;
+        cout << "Insurance added: RM " << insuranceTotal << endl; 
         cout << "Final price: RM " << finalCost << endl;
+
         cout << "Total cost of Transactions ";
-        
-        for(int i = 0; i < counter; i ++)
-            cout<< sumOfTransactions[i] << ",";
-        
+        for (int i = 0; i < counter; i++)
+            cout << sumOfTransactions[i] << ",";
         cout << endl;
-         
-        
-        
-        cout<< "You are allowed to make up to 5 transactions "<<endl;
-        cout << "do you want to make another transaction? (y/n)  ";
-        cin>> choice;
-        
-       
-        if ( counter == 2){
-            cout << "Number of Transactions excited the limit! " <<endl;
+
+        cout << "You are allowed to make up to 5 transactions " << endl;
+        cout << "Do you want to make another transaction? (y/n): ";
+        cin >> choice;
+
+        if (counter == 5) {  
+            cout << "Number of Transactions exceeded the limit! " << endl;
             break;
         }
 
-    }while (choice == 'y' || choice == 'Y');
-    
-    for (int i = 0; i < counter; i++ )
+    } while (choice == 'y' || choice == 'Y');
+
+    for (int i = 0; i < counter; i++)
         total += sumOfTransactions[i];
-    
+
     double average = total / counter;
-    cout << "Average amount to pay for " << counter << " tranactions is " << average;  
+    cout << "Average amount to pay for " << counter << " transactions is " << average;  
 
     return 0;
 }
-
